0effebe4f2d764714fc8c76d946d9daaaf8a9a85
Sanoj Hettige <sanojhettige@gmail.com>
2021-08-25 23:21:37 +0530

    Create getDatesBetweenDateRange.php

---
+++ getDatesBetweenDateRange.php
@@ -0,0 +1,13 @@
+function getDatesFromRange($first, $last, $step = '+1 day', $format = 'Y-m-d' ) {
+      $dates = array();
+      $current = strtotime($first);
+      $last = strtotime($last);
+
+      while( $current <= $last ) { 
+
+          $dates[] = date($format, $current);
+          $current = strtotime($step, $current);
+      }
+
+      return $dates;
+    }
